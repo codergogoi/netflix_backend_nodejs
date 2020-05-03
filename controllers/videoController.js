76e8b67a421fc0861f67881f560a66d9970b2a11
JAYANTA GOGOI <jayantagogoi@McCloskey.local>
2020-05-03 13:00:26 +0530

    Fixed File not founf issue

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -12,51 +12,36 @@ exports.topVideos = (req, res, next) => {
     });
 };
 
-exports.getWatchVideo = (req, res, next) => {
+exports.getWatchVideo = async (req, res, next) => {
   const movie_id = req.params.id;
 
-  Movie.findById(movie_id)
-    .then((movie) => {
-      const path = `s3_videos/${movie.fileName}`;
-      const stat = fs.statSync(path);
-      const fileSize = stat.size;
-      const range = req.headers.range;
+  const movie = await Movie.findById(movie_id);
 
-      console.log(range);
+  if (movie._id) {
+    const path = "s3_videos/" + movie.fileName;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunkSize = end - start + 1;
+      const file = fs.createReadStream(path, { start, end });
 
-      if (range) {
-        const parts = range.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-        const chunkSize = end - start + 1;
-        const file = fs.createReadStream(path, { start, end });
+      const head = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Range": "bytes",
+        "Content-Length": chunkSize,
+        "Content-Type": "video/mp4",
+      };
 
-        const head = {
-          "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-          "Accept-Range": "bytes",
-          "Content-Length": chunkSize,
-          "Content-Type": "video/mp4",
-        };
-
-        res.writeHead(206, head);
-        file.pipe(res);
-      }
-    })
-    .catch((err) => {
-      res.status(404).json("Movie Does not exist");
-    });
-
-  // if (req.userId) {
-
-  // } else {
-  //   // const head = {
-  //   //   "Content-Length": fileSize,
-  //   //   "Content-Type": "video/mp4",
-  //   // };
-  //   // res.writeHead(200, head);
-  //   // fs.createReadStream(path).pipe(res);
-  // }
-  // } else {
-  //   res.status(403).json("access denied!");
-  // }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      res.status(404).json("Movie file Does not exist");
+    }
+  } else {
+    res.status(404).json("Movie Does not exist");
+  }
 };
