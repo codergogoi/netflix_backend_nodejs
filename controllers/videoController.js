7d4372720cbaa41b0e13d616cf3523d1f738b1e4
JAYANTA GOGOI <jayantagogoi@McCloskey.local>
2020-05-20 13:00:33 +0530

    streaming 1

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -30,6 +30,37 @@ exports.viewEpisodes = (req, res, next) => {
 exports.getWatchVideo = async (req, res, next) => {
   const movie_id = req.params.id;
 
+  Movie.findById(movie_id)
+    .then((movie) => {
+      const path = "s3_videos/" + movie.fileName;
+      const stat = fs.statSync(path);
+      const fileSize = stat.size;
+      const range = req.headers.range;
+      if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+        const chunkSize = end - start + 1;
+        const file = fs.createReadStream(path, { start, end });
+
+        const head = {
+          "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+          "Accept-Range": "bytes",
+          "Content-Length": chunkSize,
+          "Content-Type": "video/mp4",
+        };
+
+        res.writeHead(206, head);
+        file.pipe(res);
+      }
+    })
+    .catch((err) => {
+      err.statusCode = 500;
+      next(err);
+    });
+
+  /*
+
   const movie = await Movie.findById(movie_id);
 
   if (movie._id) {
@@ -59,4 +90,5 @@ exports.getWatchVideo = async (req, res, next) => {
   } else {
     res.status(404).json("Movie Does not exist");
   }
+  */
 };
