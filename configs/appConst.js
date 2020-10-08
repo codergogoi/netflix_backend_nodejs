d6890094e2e99e63098b0e9d385c709df1701c01
JAYANTA GOGOI <mccloskey@JAYANTAs-MacBook-Pro.local>
2020-10-08 11:06:36 +0530

    Changed Craedntial

--- configs/appConst.js
+++ configs/appConst.js
@@ -1,4 +1,4 @@
 exports.MONGODB_URI =
-  "mongodb+srv://albert:19Albert84@cluster0-l3k2e.mongodb.net/netflix_example";
+  "mongodb+srv://youtube_tutorial:Xz4D7eBIfgEv1k5U@cluster0-l3k2e.mongodb.net/netflix_example";
 
 exports.APP_KEY = "my_app_key";
